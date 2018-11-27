6710a79a16a6316db48661ad39d7b05ff8e0ce43
zuoguozhen <34364053+zuoguozhen@users.noreply.github.com>
2018-11-27 16:22:31 +0800

    compile to implementation

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -26,10 +26,10 @@ class HugoPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.2-SNAPSHOT'
+      debugImplementation 'com.jakewharton.hugo:hugo-runtime:1.2.2-SNAPSHOT'
       // TODO this should come transitively
-      debugCompile 'org.aspectj:aspectjrt:1.8.6'
-      compile 'com.jakewharton.hugo:hugo-annotations:1.2.2-SNAPSHOT'
+      debugImplementation 'org.aspectj:aspectjrt:1.8.6'
+      implementation 'com.jakewharton.hugo:hugo-annotations:1.2.2-SNAPSHOT'
     }
 
     project.extensions.create('hugo', HugoExtension)
